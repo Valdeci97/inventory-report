40e22e71068ebdbaba8f43ff0a686ce0a59cb564
valdeci <moura97.valdeci@gmail.com>
2022-07-10 14:24:54 -0300

    feat: concludes requirement 2

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,23 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+
+expected = (
+    'O produto farinha' +
+    ' fabricado em 01-05-2021' +
+    ' por Farinini' +
+    ' com validade até 02-06-2023' +
+    ' precisa ser armazenado ao abrigo de luz.'
+)
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        7,
+        'farinha',
+        'Farinini',
+        '01-05-2021',
+        '02-06-2023',
+        '123456895',
+        'ao abrigo de luz',
+    )
+    assert str(product) == expected
+    assert isinstance(product, Product)
