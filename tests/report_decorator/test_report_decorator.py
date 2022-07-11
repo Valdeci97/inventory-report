1c28703a629e84eb2c52c23a65bfef0b722aac2c
valdeci <moura97.valdeci@gmail.com>
2022-07-11 18:44:40 -0300

    feat: create colered report tests

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,24 @@
-# from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.simple_report import SimpleReport
+
+
+products = [{
+    'id': '1',
+    'nome_do_produto': 'farinha',
+    'nome_da_empresa': 'Rosa Branca',
+    'data_de_fabricacao': '2022-07-08',
+    'data_de_validade': '2022-11-04',
+    'numero_de_serie': '1234567891',
+    'instrucao_de_armazenamento': 'em local seco e arejado',
+}]
+
+
+expected = '''\033[32mData de fabricação mais antiga:\033[0m \033[36m2022-07-08\033[0m
+\033[32mData de validade mais próxima:\033[0m \033[36m2022-11-04\033[0m
+\033[32mEmpresa com mais produtos:\033[0m \033[31mRosa Branca\033[0m'''
 
 
 def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+    colored_report = ColoredReport(SimpleReport).generate(products)
+
+    assert colored_report == expected
