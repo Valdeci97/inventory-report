6d5b3b47bb2e27fd7020871558b467ee72be3d81
valdeci <moura97.valdeci@gmail.com>
2022-07-11 15:21:17 -0300

    refactor: remove unnused parenthesis

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -9,7 +9,7 @@ class SimpleReport:
         companies = [row['nome_da_empresa'] for row in data]
         most_common_company = Counter(companies).most_common()[0][0]
         return (
-          f'Data de fabricação mais antiga: {min((fabrication_date))}\n'
-          f'Data de validade mais próxima: {min((dead_line_date))}\n'
+          f'Data de fabricação mais antiga: {min(fabrication_date)}\n'
+          f'Data de validade mais próxima: {min(dead_line_date)}\n'
           f'Empresa com mais produtos: {most_common_company}'
         )
